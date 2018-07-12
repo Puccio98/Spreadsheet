a694857b69b8c9d1ae474115dc65e7e7880f547c
davide <davide.pucci@stud.unifi.it>
2018-07-12 22:18:57 +0200

    Created min() method Fixed closing window bug

--- CLionProjects/Spreadsheet/ResultCalculation.cpp
+++ CLionProjects/Spreadsheet/ResultCalculation.cpp
@@ -4,7 +4,7 @@
 
 #include "ResultCalculation.h"
 
-ResultCalculation::ResultCalculation(MySpreadsheet* su):subjPtr(su){
+ResultCalculation::ResultCalculation(MySpreadsheet *su) : subjPtr(su) {
     cellsValues = new double[subjPtr->numOfCells];
     subjPtr->addObserver(this);
 }
@@ -15,13 +15,15 @@ ResultCalculation::~ResultCalculation() {
 
 void ResultCalculation::update() {
     wxString s;
-    for(int i = 0; i < subjPtr->numOfCells;i++){
+
+    for (int i = 0; i < subjPtr->numOfCells; i++) {
         s = subjPtr->cells[i]->GetValue();
         s.ToDouble(&cellsValues[i]);
 
-        if(subjPtr->cells[i]->GetValue() == wxEmptyString)
+        if (subjPtr->cells[i]->GetValue() == wxEmptyString)
             cellsValues[i] = 0;
     }
+
     double somma = sum();
     media(somma);
     max();
@@ -31,10 +33,12 @@ void ResultCalculation::update() {
 
 double ResultCalculation::sum() {
     double sum = 0;
-    for(int i = 0; i < 60;i++)
-        if(subjPtr->cells[i]->GetValue() != wxT("-"))
+
+    for (int i = 0; i < 60; i++)
+        if (checkString(i))
             sum = sum + cellsValues[i];
-    wxString str = wxString::Format(wxT("%lf"),sum);
+
+    wxString str = wxString::Format(wxT("%lf"), sum);
     subjPtr->results[0]->ChangeValue(str);
 
     return sum;
@@ -42,12 +46,12 @@ double ResultCalculation::sum() {
 
 void ResultCalculation::media(double sum) {
     int count = 0;
-    for(int i = 0; i < subjPtr->numOfCells;i++){
-        if(subjPtr->cells[i]->GetValue() != wxEmptyString && subjPtr->cells[i]->GetValue() != wxT("-"))
+    for (int i = 0; i < subjPtr->numOfCells; i++) {
+        if (checkString(i))
             count++;
     }
 
-    if(count != 0) {
+    if (count != 0) {
         double media = sum / count;
         wxString str = wxString::Format(wxT("%lf"), media);
         subjPtr->results[1]->ChangeValue(str);
@@ -57,33 +61,68 @@ void ResultCalculation::media(double sum) {
 }
 
 void ResultCalculation::max() {
-    double max;
+
+    double max = 0;
     bool b = true;
-    int i = 0;
-    while(b){
-        if(subjPtr->cells[i]->GetValue() != wxEmptyString){
+
+    for (int i = 0; i < subjPtr->numOfCells && b; i++) {
+        if (checkString(i)) {
             max = cellsValues[i];
             b = false;
         }
-        i++;
     }
 
-    for(i = 0;i < subjPtr->numOfCells; i++){
-        if(subjPtr->cells[i]->GetValue() != wxEmptyString)
-            if(cellsValues[i] > max)
-                max = cellsValues[i];
-    }
+    if (!b) {
+        for (int i = 0; i < subjPtr->numOfCells; i++) {
+            if (checkString(i))
+                if (cellsValues[i] > max)
+                    max = cellsValues[i];
+        }
 
-    wxString str = wxString::Format(wxT("%lf"),max);
-    subjPtr->results[2]->ChangeValue(str);
+        wxString str = wxString::Format(wxT("%lf"), max);
+        subjPtr->results[2]->ChangeValue(str);
+
+    } else
+        subjPtr->results[2]->ChangeValue(wxT("No values"));
+
 }
 
 void ResultCalculation::min() {
+
     double min = 0;
+    bool b = true;
+
+    for (int i = 0; i < subjPtr->numOfCells && b; i++) {
+        if (checkString(i)) {
+            min = cellsValues[i];
+            b = false;
+        }
+    }
+
+    if (!b){
+        for(int i = 0; i < subjPtr->numOfCells; i++){
+            if(checkString(i)){
+                if(cellsValues[i] < min)
+                    min = cellsValues[i];
+            }
+        }
+
+        wxString str = wxString::Format(wxT("%lf"), min);
+        subjPtr->results[3]->ChangeValue(str);
+
+    } else
+        subjPtr->results[3]->ChangeValue(wxT("No values"));
+
 }
 
 bool ResultCalculation::checkString(int i) {
 
+    wxString str = subjPtr->cells[i]->GetValue();
+
+    if (!str.IsEmpty() && str != wxT("-"))
+        return true;
+    else
+        return false;
 }
 
 

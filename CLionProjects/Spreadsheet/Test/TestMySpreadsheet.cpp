6274637074267675095369e025f2e11ee9c50515
davide <davide.pucci@stud.unifi.it>
2018-07-14 01:36:46 +0200

    Gtest working, cmakelist problems solved

--- CLionProjects/Spreadsheet/Test/TestMySpreadsheet.cpp
+++ CLionProjects/Spreadsheet/Test/TestMySpreadsheet.cpp
@@ -2,10 +2,23 @@
 // Created by davide on 12/07/18.
 
 #include "gtest/gtest.h"
+#include "wx/wx.h"
 #include "../MySpreadsheet.h"
+#include "../main.h"
 
-TEST(TestMySpreadsheet,test){
-    auto ptr = new MySpreadsheet("window");
+
+class MySpreadsheetSuite : public testing::Test{
+public:
+    void SetUp() override {
+
+    }
+};
+
+TEST_F(MySpreadsheetSuite,MySpreadsheetConstructor){
+
+    std::unique_ptr<MySpreadsheet> ptr (new MySpreadsheet(wxT("window")));
     ASSERT_EQ(wxSize(720,480),ptr->GetSize());
+
+
 }
 
